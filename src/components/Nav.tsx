d08061f690a11696693cb38738cb3250a2b2cb43
Valeriy <masterbox06@gmail.com>
2023-07-30 18:36:15 +0400

    app created, grid template done

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <div><a href="/profile">Profile</a></div>
+      <div><a href="/messages">Messages</a></div>
+      <div><a href="/news">News</a></div>
+      <div><a href="/music">Music</a></div>
+      <div><a href="/settings">Settings</a></div>
+    </nav>
+  );
+};
+
+export default Nav;
