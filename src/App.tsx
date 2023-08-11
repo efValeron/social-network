63e49b1c7df6065382c1eb49c0b81b3a3a7ea74d
Valeriy <masterbox06@gmail.com>
2023-08-11 16:35:06 +0400

    added router funcs

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import {Profile} from "./components/Profile/Profile";
 import {Messages} from "./components/Messages/Messages";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="box-border p-2 h-full font-hel">
-      <Header/>
-      <div className="mt-2 grid grid-cols-12 gap-2">
-        <Nav/>
-        <div className="bg-sky-500 col-span-11 p-2 box-border">
-          {/*<Profile/>*/}
-          <Messages/>
+    <BrowserRouter>
+      <div className="box-border p-2 h-full font-hel">
+        <Header/>
+        <div className="mt-2 grid grid-cols-12 gap-2">
+          <Nav/>
+          <div className="bg-sky-500 col-span-11 p-2 box-border">
+            <Routes>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/messages" element={<Messages/>}/>
+              <Route path="/news" element={<News/>}/>
+              <Route path="/music" element={<Music/>}/>
+              <Route path="/settings" element={<Settings/>}/>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
