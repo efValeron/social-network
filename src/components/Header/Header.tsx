df75726355c45470d105362066394b732f280ee4
Valeriy <masterbox06@gmail.com>
2023-08-11 15:34:55 +0400

    rework component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="h-8 bg-green-500">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+           alt="img"
+           className="w-6 h-6"
+      />
+    </header>
+  );
+};
+
+export default Header;
