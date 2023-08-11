ff270f89550e7248678d4775338e114497344419
Valeriy <masterbox06@gmail.com>
2023-08-11 15:33:37 +0400

    reassembled App, added profile component usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './App.css'
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Profile from "./components/Profile";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import {Profile} from "./components/Profile/Profile";
+import {Messages} from "./components/Messages/Messages";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
+    <div className="box-border p-2 h-full font-hel">
+      <Header/>
+      <div className="mt-2 grid grid-cols-12 gap-2">
+        <Nav/>
+        <div className="bg-sky-500 col-span-11 p-2 box-border">
+          {/*<Profile/>*/}
+          <Messages/>
+        </div>
+      </div>
     </div>
   );
 };
