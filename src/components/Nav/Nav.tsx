013c454885c2ba35c83ec475bb8890a3d4c7fa41
Valeriy <masterbox06@gmail.com>
2023-08-11 16:37:26 +0400

    added route func, a tag changed to NavLink

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="flex flex-col col-span-1 bg-amber-500 font-semibold text-stone-50 box-border p-2 gap-2">
-      <div><a href="/profile">Profile</a></div>
-      <div><a href="/messages">Messages</a></div>
-      <div><a href="/news">News</a></div>
-      <div><a href="/music">Music</a></div>
-      <div><a href="/settings">Settings</a></div>
+      <div><NavLink className={({isActive}) => isActive ? "text-stone-300" : ""}
+                    to="/profile">Profile</NavLink></div>
+
+      <div><NavLink className={({isActive}) => isActive ? "text-stone-300" : ""}
+                    to="/messages">Messages</NavLink></div>
+
+      <div><NavLink className={({isActive}) => isActive ? "text-stone-300" : ""}
+                    to="/news">News</NavLink></div>
+
+      <div><NavLink className={({isActive}) => isActive ? "text-stone-300" : ""}
+                    to="/music">Music</NavLink></div>
+
+      <div><NavLink className={({isActive}) => isActive ? "text-stone-300" : ""}
+                    to="/settings">Settings</NavLink></div>
     </nav>
   );
 };
