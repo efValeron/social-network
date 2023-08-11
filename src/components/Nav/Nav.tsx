6ce3a0acab9015422e391704a6a4ac32ae753a7d
Valeriy <masterbox06@gmail.com>
2023-08-11 15:35:56 +0400

    removed unnecessary styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav className="nav">
+    <nav className="flex flex-col col-span-1 bg-amber-500 font-semibold text-stone-50 box-border p-2 gap-2">
       <div><a href="/profile">Profile</a></div>
       <div><a href="/messages">Messages</a></div>
       <div><a href="/news">News</a></div>
