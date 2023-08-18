d815d299370e02afb724a8ff3c6962a865a831b9
Valeriy <masterbox06@gmail.com>
2023-08-18 23:30:19 +0400

    improved profile styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import Posts from "./Posts/Posts";
+import profileBGImage from "../../images/profileBGImage.jpg";
+import profileImage from "../../images/profileImage.jpg";
 
 export const Profile = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <img
-        src="https://img.freepik.com/free-photo/view-new-york-city-usa_53876-153448.jpg?w=1380&t=st=1690973433~exp=1690974033~hmac=64bece425098fe687e4f4b8dc699cac9335c4a1b28827857976744f78946261c"
-        alt="img"
-        className="mx-auto h-64 object-none w-full"
-      />
+
+    <div className="flex flex-col gap-4 px-4 box-border rounded-t-3xl">
+      <div className="h-96 w-full overflow-hidden  rounded-b-md realtive">
+        <img
+          src={profileBGImage}
+          alt="img"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex flex-row gap-4">
+        <div className="rounded-full bg-blue-200 h-48 w-48 overflow-hidden">
+          <img
+            src={profileImage}
+            alt="img"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="box-border p-2">
+          <h1 className="text-3xl font-semibold">Saleh Ahmed</h1>
+          <p className="mt-2 text-lg font-medium opacity-60">UI Designer</p>
+        </div>
+        <button className="bg-slate-100 hover:bg-slate-200 duration-100 rounded-lg min-h-8 p-4">Edit profile</button>
+      </div>
       <div className="flex flex-row gap-4">
         <div className="w-8 h-8 bg-fuchsia-500"></div>
         <div>
           about
         </div>
       </div>
-      <Posts />
+      <Posts/>
     </div>
   );
 };
